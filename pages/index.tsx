dbcfe65a8827f83503b81fbe224123e78b1a29c5
yoon1139 <yoon_1139@naver.com>
2022-04-14 23:03:26 +0900

    parallel queries, dependent queries, caching

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { Fragment } from "react";
+import Link from "next/link";
 
 interface Post {
   id: number;
@@ -21,7 +22,12 @@ const Home: NextPage = () => {
     isLoading,
     isError,
     error,
-  } = useQuery<Post[], Error>("posts", getPosts);
+  } = useQuery<Post[], Error>("posts", getPosts, {
+    staleTime: 3 * 1000,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    refetchInterval: false,
+  });
 
   if (isError) {
     return <div>{error.message}</div>;
@@ -29,16 +35,34 @@ const Home: NextPage = () => {
 
   return (
     <>
+      <nav style={{ display: "flex" }}>
+        <Link href="/parallel">
+          <a style={{ marginRight: "1rem" }}>Parallel Queries Page</a>
+        </Link>
+
+        <Link href="/dependent">
+          <a style={{ marginRight: "1rem" }}>Dependent Queries Page</a>
+        </Link>
+      </nav>
+
+      <br />
+
       <div>
         {isLoading ? (
           <div>Loading...</div>
         ) : (
           posts?.map((post) => (
             <Fragment key={post.id}>
-              <div>id: {post.id}</div>
-              <div>제목: {post.title}</div>
-              <div>작성자: {post.author}</div>
-              <div>내용: {post.description.slice(0, 100)}...</div>
+              <br />
+              <Link href={`/post/${post.id}`}>
+                <a>
+                  <div>id: {post.id}</div>
+                  <div>제목: {post.title}</div>
+                  <div>작성자: {post.author}</div>
+                  <div>내용: {post.description.slice(0, 100)}...</div>
+                </a>
+              </Link>
+              <br />
               <hr />
             </Fragment>
           ))
